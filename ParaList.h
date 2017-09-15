d1a81917d155883ab3c5e65538b2cf4592c8cb8d
leevenlin <venling@foxmail.com>
2017-09-15 22:32:59 +0800

    Add files via upload

---
+++ ParaList.h
@@ -0,0 +1,15 @@
+#pragma once;
+
+extern struct QT_ParaSet
+{
+	unsigned int sampleRate;
+	unsigned int inputRange;
+	float sampleRange;
+	unsigned int angleStart;
+	unsigned int angleEnd;
+	char fileName;
+	char fileLocation;
+	bool saveFile;
+};
+
+extern 
